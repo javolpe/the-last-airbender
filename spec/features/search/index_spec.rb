00a9afe3650a4a2eb6cfdf258d7a5cc3ffe80ff3
Jake Volpe <volpe.jacob@gmail.com>
2021-04-05 09:25:33 -0600

    Add display nation name and total members to test, pass previous test

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Search by Nation index page' do 
   before :each do 
+    @total_members ||=AirbenderService.total_members("Fire Nation")
     visit root_path
     select "Fire Nation", :from => "nation"
     click_button "Search For Members"
   end
-  it "takes you to the Search For Members by nation index page" do 
-    expect(current_path).to eq(search_path)
-  end
+  describe "When I visit the Search index page for a specific nation" do 
+    it "takes you to the Search For Members by nation index page" do 
+      expect(current_path).to eq(search_path)
+    end
+    it "shows the nation you searched for and the total members" do 
+      within "#nation" do 
+        expect(page).to have_content("Members of the Fire Nation")
+      end
+      withing "#totalMembers" do 
+        expect(page).to have_content("Total Members: #{@total_members}")
+      end
+    end
 
 end
